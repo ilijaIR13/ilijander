02d4d53643e0b79583f45981a1985eb4c0fb1cab
ilijaIR13 <77515537+ilijaIR13@users.noreply.github.com>
2025-09-18 00:35:26 +0200

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,12 @@
-// src/lib/supabase.ts
-import { createClient } from "@supabase/supabase-js";
+
+'use client'
+
+import { createBrowserClient } from '@supabase/ssr'
 
 export function browserSupabase() {
-  const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-  const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-  return createClient(url, key);
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
 }
+
