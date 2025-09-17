ae7ba66b190df02a4b0588b9b6fda5281b582377
ilijaIR13 <77515537+ilijaIR13@users.noreply.github.com>
2025-09-18 00:37:43 +0200

    Update Container.tsx

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,3 +1,6 @@
-export default function Container({ children }: { children: React.ReactNode }) {
-  return <div className="mx-auto max-w-5xl px-4 py-6">{children}</div>;
+
+import { ReactNode } from 'react'
+export default function Container({ children }: { children: ReactNode }) {
+  return <div className="mx-auto max-w-4xl px-4 sm:px-6 lg:px-8">{children}</div>
 }
+
